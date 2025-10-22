7b1b0d6f89d82a63a546b577ace4876abc2d4aed
Kristian Carbonaro <carbonarok@gmail.com>
2025-10-22 14:40:02 +0100

    Updated install

--- nvim/lua/plugins/supermaven.lua
+++ nvim/lua/plugins/supermaven.lua
@@ -1,6 +1,7 @@
 return {
-  'supermaven-inc/supermaven-nvim',
-  config = function()
-    require('supermaven-nvim').setup {}
-  end,
+	"supermaven-inc/supermaven-nvim",
+	config = function()
+		require("supermaven-nvim").setup({})
+		vim.cmd.SuperMavenUseFree()
+	end,
 }
